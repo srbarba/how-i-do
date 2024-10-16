1907c942a1441e2dcf0aeb86daafda2d71939228
JuanP Barba <jpbs92@hotmail.com>
2024-10-16 04:51:52 +0200

    fix: 🐛 add vue support to astro

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,14 @@
 import tutorialkit from '@tutorialkit/astro'
 import { defineConfig } from 'astro/config'
 
+import vue from '@astrojs/vue'
+
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
   integrations: [
+    vue(),
     tutorialkit({
       components: {
         TopBar: './src/components/TopBar.astro',
